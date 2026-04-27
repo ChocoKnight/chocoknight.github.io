b6964580cdb2435e12a2e1c15e7fcfd5951f16d7
Kevin Tan <k.tann.578@gmail.com>
2026-04-26 17:27:11 -0700

    develop - icons

--- src/components/header.js
+++ src/components/header.js
@@ -15,10 +15,12 @@ export class HeaderElement extends HTMLElement {
                   <h1><a href="/">KEVIN TAN</a></h1>
               </div>
               <div class="nav-item">
-                  <h1><a href="/cv/">RESUME/CV</a></h4>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-user-icon lucide-file-user"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M16 22a4 4 0 0 0-8 0"/><circle cx="12" cy="15" r="3"/></svg>
+                  <h1><a href="/cv/">RESUME/CV</a></h1>
               </div>
               <div class="nav-item">
-                <h1><a href="/projects/">PROJECTS</a></h4>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-icon lucide-list"><path d="M3 5h.01"/><path d="M3 12h.01"/><path d="M3 19h.01"/><path d="M8 5h13"/><path d="M8 12h13"/><path d="M8 19h13"/></svg>
+                <h1><a href="/projects/">PROJECTS</a></h1>
               </div>
           </div>
         <hr>
